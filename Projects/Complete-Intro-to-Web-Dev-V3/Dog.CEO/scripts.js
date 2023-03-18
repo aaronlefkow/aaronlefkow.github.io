cc2e1d2e8dc137eae39d6fc5762d3c86c223b454
Aaron Lefkow <aaronlefkow@gmail.com>
2023-03-17 19:55:30 -0500

    100% width

--- Projects/Complete-Intro-to-Web-Dev-V3/Dog.CEO/scripts.js
+++ Projects/Complete-Intro-to-Web-Dev-V3/Dog.CEO/scripts.js
@@ -13,7 +13,9 @@ function addNewDoggo() {
     const img = document.createElement("img");
     img.src = dogObject.message;
     img.alt = "Cute doggo";
-    
+    img.style.maxWidth = "100%";
+    img.style.height = "auto";
+
     const oldImg = doggos.querySelector("img");
     if (oldImg) {
       doggos.removeChild(oldImg);
